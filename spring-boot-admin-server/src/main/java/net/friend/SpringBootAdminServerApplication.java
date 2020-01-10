49976cf8a899142150e973b5333b767bfccd1083
Quan Bui <quanbh.hust@gmail.com>
2020-01-10 10:10:27 +0700

    configure spring boot admin and client

---
+++ spring-boot-admin-server/src/main/java/net/friend/SpringBootAdminServerApplication.java
@@ -0,0 +1,14 @@
+package net.friend;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class SpringBootAdminServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootAdminServerApplication.class);
+  }
+}
